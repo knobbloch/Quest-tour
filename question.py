6d05dd50629f114292051ccf5825346e609ea6d7
knobbloch <knobbloch@gmail.com>
2024-03-10 23:59:24 +0500

    serealization

---
+++ question.py
@@ -0,0 +1,12 @@
+class Question():
+    def __init__(self, question, answers, right_answers):
+        self.question = question    
+        self.answers = answers    
+        self.right_answers = right_answers      
+    question: str
+    answers: list
+    right_answers: list
+
+
+
+
