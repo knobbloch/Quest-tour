8fb66fa4ee476fe2fcc1ed9682a23a643ffdbc52
KosichkaCheese <selivanovaalina9@gmail.com>
2024-03-12 20:41:02 +0500

    добавила функцию для выбора вопроса

--- main.py
+++ main.py
@@ -6,30 +6,42 @@ from question import Question
 
 app = FastAPI()
 
-object = Question("gay?", ["yes", "yess", "yess", "no", "?"], ["yes", "yess", "yess"]) #первый обьект
-pickled_object = pickle.dumps(object)   #сереализация к виду x80\\x04\\x95j\\x00\\x00\\x00\\x00\\x00\\x00
-unpickled_object = pickle.loads(pickled_object) #десереализация
-object2 = Question("your mama gay?", ["yes", "yess", "-", "no", "?"], ["yes", "yess"])#второй обьект
+object = Question("gay?", ["yes", "yess", "yess", "no", "?"], ["yes", "yess", "yess"])  # первый обьект
+pickled_object = pickle.dumps(object)  # сереализация к виду x80\\x04\\x95j\\x00\\x00\\x00\\x00\\x00\\x00
+unpickled_object = pickle.loads(pickled_object)  # десереализация
+object2 = Question("your mama gay?", ["yes", "yess", "-", "no", "?"], ["yes", "yess"])  # второй обьект
 
-list_object = [object, object2] #список
+list_object = [object, object2]  # список
 
-file = open("data/test.txt", "wb") #запись в файл
-pickle.dump (list_object, file)#сереализация
+file = open("data/test.txt", "wb")  # запись в файл
+pickle.dump(list_object, file)  # сереализация
 file.close()
+
 
 @app.get("/script")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.post("/script/write_to_file")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/script/read_from_file")
 async def root():
     file2 = open("data/test.txt", "rb")
-    readed_list = pickle.load(file2)#десереализация
+    readed_list = pickle.load(file2)  # десереализация
     file2.close()
     return {"message": readed_list[0].question, "message2": readed_list[1].question}
+
+
+@app.get("/script/read_test_from_file")
+async def root(question_number: int):
+    file2 = open("data/test.txt", "rb")
+    question_list = pickle.load(file2)  # десереализация
+    file2.close()
+    return question_list[question_number-1]
+
 
 app.mount("/", StaticFiles(directory="front", html=True), name="front")
