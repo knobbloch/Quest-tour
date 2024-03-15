d3452e6658cf1164b2c5911883812371c3660089
KosichkaCheese <selivanovaalina9@gmail.com>
2024-03-16 00:33:16 +0500

    changed answer check function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
 from starlette.staticfiles import StaticFiles
@@ -13,12 +13,17 @@ object = Question("Ты gay?", ["yes", "yess", "yess", "no", "?"], [0, 1, 2])  #
 # pickled_object = pickle.dumps(object)  # сереализация к виду x80\\x04\\x95j\\x00\\x00\\x00\\x00\\x00\\x00
 # unpickled_object = pickle.loads(pickled_object)  # десереализация
 object2 = Question("your mama gay?", ["yes", "yess", "-", "no", "?"], [0, 1])  # второй обьект
+object3 = Question("Am I gay?", ["of course", "why", "14"], [2])
 
-list_object = [object, object2]  # список
+list_object = [object, object2, object3]  # список
 
 file = open("data/test.txt", "wb")  # запись в файл
 pickle.dump(list_object, file)  # сереализация
 file.close()
+
+
+class List_of_str(BaseModel):
+    sections: List[str]
 
 
 @app.get("/script")
@@ -55,25 +60,23 @@ async def read():
 
 
 @app.post("/script/test_post")
-async def test(text: str):
+async def test(text: List_of_str):
     return text
 
 
-class List_of_list(BaseModel):
-    sections: List[List[int]]
-
-
 @app.post("/script/send_answers", response_model=int)
-async def send_answer(answer_list: list = Query()):
+async def send_answer(answer_list: List_of_str):
     right_answer_list = []
     counter = 0
     questions = read_file_test("data/test.txt")
     new_answers = []
-    for i in answer_list:
+    print(answer_list.sections)
+    for i in answer_list.sections:
         answer = i.split(", ")
         for j in range(0, len(answer)):
             answer[j] = int(answer[j])
         new_answers.append(answer)
+    print(new_answers)
 
     for i in questions:
         right_answer_list.append(i.right_answers)
