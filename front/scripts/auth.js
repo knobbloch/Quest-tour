73f1fb57f54ea5dfc26e4dccb5c760bc3b8a932d
Jale <jalencess@gmail.com>
2024-05-13 00:46:57 +0500

    аутхххххххххххххххххххххххххх

--- front/scripts/auth.js
+++ front/scripts/auth.js
@@ -1,5 +1,9 @@
 var email;
 var password;
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+var continue1 = urlParams.get('continue')
+console.log(continue1)
 
 document.getElementById("sign-in-btn").addEventListener("click", function() {
     email = document.getElementById("email").value;
@@ -24,9 +28,15 @@ async function SendDataToServer(email, password) {
         if (response.status < 200 || response.status >= 300) {
             throw new Error("Ошибка");
         }
-
+        console.log(continue1)
+        console.log(urlParams)
         const data = await response.data;
-        window.location.href = 'http://127.0.0.1:8000/map.html';
+        if (continue1 != null && continue1 != "/lecture.html" && continue1 != "/test.html" && continue1 != "/test_result.html"){
+            window.location.href = 'http://127.0.0.1:8000' + continue1;
+        }
+        else{
+             window.location.href = 'http://127.0.0.1:8000/map.html';
+        }
         document.getElementById("error-message").style.display = "none";
 
         return data;
