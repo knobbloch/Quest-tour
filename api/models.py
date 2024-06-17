9cc76402d22609b6dbec39cac735c1c0bf95182a
KosichkaCheese <selivanovaalina9@gmail.com>
2024-06-17 23:33:34 +0500

    edit lecture edit

--- api/models.py
+++ api/models.py
@@ -60,6 +60,14 @@ class Lecture(BaseModel):
 class EditLecture(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
+    pathto: Optional[str] = None
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_to_json(cls, value):
+        if isinstance(value, str):
+            return cls(**json.loads(value))
+        return value
 
 
 class Practice(BaseModel):
