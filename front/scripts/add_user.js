f786f9b6dc98697c7d99ed84df1af4e368a4c32f
Facenip <122119233+Facenip@users.noreply.github.com>
2024-06-19 21:40:52 +0500

    added foolproof

--- front/scripts/add_user.js
+++ front/scripts/add_user.js
@@ -7,38 +7,68 @@ let surname_inf = document.getElementById("surname"),
   department_inf = document.getElementById("department"),
   login_inf = document.getElementById("login");
 
-  async function sendInfToServer(login,surname,name,midname,city,department) { 
-    const URL = `${window.location.origin}/script/add_user`;
-    axios({
-      method: 'post',
-      url: URL,
-      data: {
-        email: login,
-        namep: name,
-        surname: surname,
-        thirdname: midname,
-        division: department,
-        city: city
-      }
-    })
-    .then(response => {
-      console.log(response.data)
-      if(response.data['status']!=201){
-        document.getElementById("modal__box-text").textContent = "Возникла ошибка :( Попробуйте ещё раз";
-      }
-      document.getElementById("exit-modal-ok").classList.add("open")
-    })
-    .catch(error => {
-      console.log(error)
-    })
+function destroy_gap(str){
+  let i = 0;
+  while(str[i] === " " && i < str.length){
+    i++;
   }
+  return str.slice(i);
+}
+
+function no_num(obj){
+  obj.value = destroy_gap(obj.value);
+  if(/^[A-Za-zА-Яа-яЁё\s]+$/.test(obj.value)){
+    obj.classList.remove("error");
+    return true;
+  }else{
+    obj.classList.add("error");
+    return false;
+  };
+}
+
+function is_email(obj){
+  obj.value = destroy_gap(obj.value);
+  if(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(obj.value)){
+    obj.classList.remove("error");
+    return true;
+  }else{
+    obj.classList.add("error");
+    return false;
+  }
+}
+
+async function sendInfToServer(login,surname,name,midname,city,department) { 
+  const URL = `${window.location.origin}/script/add_user`;
+  axios({
+    method: 'post',
+    url: URL,
+    data: {
+      email: login,
+      namep: name,
+      surname: surname,
+      thirdname: midname,
+      division: department,
+      city: city
+    }
+  })
+  .then(response => {
+    console.log(response.data)
+    if(response.data['status']!=201){
+      document.getElementById("modal__box-text").textContent = "Возникла ошибка :( Попробуйте ещё раз";
+    }
+    document.getElementById("exit-modal-ok").classList.add("open")
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
 
 function change_inf(){
-  if(login_inf.value!="" && surname_inf.value!="" && name_inf.value!=""){
+  if(no_num(surname_inf) * no_num(name_inf) * no_num(midname_inf) * no_num(city_inf) * no_num(department_inf)* is_email(login_inf)){
     error.textContent="";
     sendInfToServer(login_inf.value,surname_inf.value,name_inf.value,midname_inf.value,city_inf.value,department_inf.value);
   }else{
-    error.textContent="*Заполните все обязательные поля!";
+    error.textContent="Корректно заполните все поля!";
   }
 }
 
